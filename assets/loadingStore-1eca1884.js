f5b59bb1ca18571cc5d94cf41f2ea475d81142db
morning <nmb467532000@gmail.com>
2025-02-02 14:48:37 +0900

    Updates

--- assets/loadingStore-1eca1884.js
+++
@@ -1,1 +0,0 @@
-import{C as i}from"./index-61cd3642.js";const t=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},600)}}});export{t as l};
---
+++ assets/loadingStore-31760bfc.js
@@ -0,0 +1,1 @@
+import{C as i}from"./index-487e050f.js";const t=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loading(){this.isLoading=!0,setTimeout(()=>{this.isLoading=!1},600)}}});export{t as l};
